d6db1ffc35cf363f7d2a412fa5c390c1a575ff44
GiovanniVente5 <giovanniVentecinco06@Gmail.com>
2025-03-16 13:59:35 -0300

    Add Animais and Cachorro classes with basic functionality

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import Classes.Animais;
+import Classes.Cachorro;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("ola");
+        Cachorro cachorro = new Cachorro("Doguinho",4,"woof");
+        System.out.println(cachorro.toString());
+
+        Animais animal = new Animais("Largatixa",4);
+        System.out.println("-------------------------------------");
+        System.out.println(animal.toString());
     }
 }
